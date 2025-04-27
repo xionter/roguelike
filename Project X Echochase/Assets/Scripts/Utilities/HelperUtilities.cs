9d9f95b0d7daea033ff6a40135ffa8e8f1665d20
xionter <andrey0502255@gmail.com>
2025-04-27 12:35:12 +0500

    aim weapon almost done

--- Project X Echochase/Assets/Scripts/Utilities/HelperUtilities.cs
+++ Project X Echochase/Assets/Scripts/Utilities/HelperUtilities.cs
@@ -4,6 +4,79 @@ using UnityEngine;
 
 public static class HelperUtilities
 {
+    public static Camera mainCamera; 
+
+    /// <summary> 
+    /// Получить позицию курсора 
+    /// </summary> 
+    public static Vector3 GetMouseWorldPosition() { 
+        if (mainCamera == null) mainCamera = Camera.main; 
+        Vector3 mouseScreenPosition = Input.mousePosition; 
+        // Ограничиваем позицию размером экрана
+        mouseScreenPosition.x = Mathf.Clamp(mouseScreenPosition.x, 0f, Screen.width); 
+        mouseScreenPosition.y = Mathf.Clamp(mouseScreenPosition.y, 0f, Screen.height); 
+        Vector3 worldPosition = mainCamera.ScreenToWorldPoint(mouseScreenPosition); 
+
+        worldPosition.z = 0f; 
+        return worldPosition; 
+    } 
+
+    /// <summary> 
+    /// Получить угол в градусах с направления вектора 
+    /// </summary> 
+    public static float GetAngleFromVector(Vector3 vector) 
+    { 
+        var radians = Mathf.Atan2(vector.y, vector.x); 
+
+        var degrees = radians * Mathf.Rad2Deg; 
+
+        return degrees;
+    }
+
+
+    public static AimDirection GetAimDirection(float angleDegrees) 
+    { 
+        AimDirection aimDirection; 
+        //UpRight 
+        if (angleDegrees >= 22f && angleDegrees <= 67f) 
+        { 
+            aimDirection = AimDirection.UpRight; 
+        } 
+        // Up 
+        else if (angleDegrees > 67f && angleDegrees <= 112f) 
+        { 
+            aimDirection = AimDirection.Up; 
+        }
+        // Up Left 
+        else if (angleDegrees > 112f && angleDegrees <= 158f) 
+        { 
+            aimDirection = AimDirection.UpLeft; 
+        }
+        // Left 
+        else if ((angleDegrees <= 180f && angleDegrees > 158f) || 
+                (angleDegrees > -180 && angleDegrees <= -135f)) 
+        { 
+            aimDirection = AimDirection.Left;
+        }
+        // Down 
+        else if (angleDegrees > -135f && angleDegrees <= -45f) 
+        { 
+            aimDirection = AimDirection.Down; 
+        }
+
+        // Right 
+        else if ((angleDegrees > -45f && angleDegrees <= 0f) || 
+                (angleDegrees > 0f && angleDegrees < 22f)) 
+        { 
+            aimDirection = AimDirection.Right;
+        }
+        else
+        {
+            aimDirection = AimDirection.Right;
+        }
+
+        return aimDirection;
+    }
     public static bool ValidateCheckNullValue(Object thisObject, string fieldName, UnityEngine.Object objectToCheck) 
     { 
         if (objectToCheck == null) 
