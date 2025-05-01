dba79730765d51c80e68deb36e4038cf1dce874b
srolket <sveta.shishkina365@gmail.com>
2025-05-01 11:06:38 +0500

    add rooms to scriptable objects

--- Project X Echochase/Assets/Scripts/Enums/Enums.cs
+++ Project X Echochase/Assets/Scripts/Enums/Enums.cs
@@ -1,16 +1,7 @@
 using UnityEngine;
 
-public class Enums : MonoBehaviour
+public enum Orientation
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    north,
+    south
 }
