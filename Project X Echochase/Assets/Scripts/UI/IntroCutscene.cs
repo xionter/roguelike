bdfe9d6de6142af69fbe92f814c8d3eaa2eed840
xionter <andrey0502255@gmail.com>
2025-05-31 20:34:56 +0500

    final merge

---
+++ Project X Echochase/Assets/Scripts/UI/IntroCutscene.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroCutscene : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private string animationName = "IntroAnimation";
+    [SerializeField] private float delayAfterAnimation = 1f;
+    [SerializeField] private bool skipCutscene = false;
+    
+    private bool isCutscenePlaying = false;
+    private float cutsceneDuration = 35f; 
+
+    private void Start()
+    {
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        if (skipCutscene)
+        {
+            LoadGameScene();
+            return;
+        }
+
+        PlayCutscene();
+    }
+
+    private void Update()
+    {
+        if (isCutscenePlaying && Input.GetKeyDown(KeyCode.Return))
+        {
+            SkipCutscene();
+        }
+    }
+
+    private void PlayCutscene()
+    {
+        isCutscenePlaying = true;
+        
+        if (animator != null && !string.IsNullOrEmpty(animationName))
+        {
+            animator.Play(animationName, -1, 0f);
+            Invoke("LoadGameScene", cutsceneDuration + delayAfterAnimation);
+        }
+        else
+        {
+            LoadGameScene(); 
+        }
+    }
+
+    private void LoadGameScene()
+    {
+        if (!isCutscenePlaying) return;
+        
+        isCutscenePlaying = false;
+        SceneManager.LoadScene("MainGameScene");
+    }
+
+    public void SkipCutscene()
+    {
+        if (!isCutscenePlaying) return;
+        
+        CancelInvoke("LoadGameScene");
+        LoadGameScene();
+        
+        if (animator != null)
+        {
+            animator.SetTrigger("Skip");
+        }
+    }
+}
--- Project X Echochase/Assets/Scripts/UI/MainMenuUI.cs
+++ Project X Echochase/Assets/Scripts/UI/MainMenuUI.cs
@@ -1,66 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
 {
-    #region Header OBJECT REFERENCES
-    [Space(10)]
-    [Header("РЕФЕРЕНСЫ ОБЪЕКТОВ")]
-    #endregion Header OBJECT REFERENCES
-    #region Tooltip
-    [Tooltip("Кнопка старта")]
-    #endregion Tooltip
     [SerializeField] private GameObject playButton;
-    #region Tooltip
-    [Tooltip("Кнопка выхода")]
-    #endregion
     [SerializeField] private GameObject quitButton;
-    #region Tooltip
-    [Tooltip("Кнопка возвращения в главное меню")]
-    #endregion
     [SerializeField] private GameObject returnToMainMenuButton;
 
     private void Start()
     {
         MusicManager.Instance.PlayMusic(GameResources.Instance.mainMenuMusic, 0f, 2f);
-
-        SceneManager.LoadScene("InitialScene", LoadSceneMode.Additive);
-
+        SceneManager.LoadScene("MainMenuScene", LoadSceneMode.Additive);
         returnToMainMenuButton.SetActive(false);
     }
 
-
-    /// <summary>
-    /// вызывается из Play Game / Enter The Dungeon
-    /// </summary>
     public void PlayGame()
     {
+        // Option 1: Go straight to game
         SceneManager.LoadScene("MainGameScene");
+
+        // Option 2: Play intro cutscene again before game (if desired)
+        SceneManager.LoadScene("IntroCutscene");
     }
 
-    /// <summary>
-    /// вызывается из Return To Main Menu
-    /// </summary>
     public void LoadInitial()
     {
         returnToMainMenuButton.SetActive(false);
         playButton.SetActive(true);
         quitButton.SetActive(true);
-        SceneManager.LoadScene("InitialScene", LoadSceneMode.Additive);
+        SceneManager.LoadScene("MainMenuScene", LoadSceneMode.Additive);
     }
-
 
     public void QuitGame()
     {
         Application.Quit();
     }
 
-
-    #region Validation
 #if UNITY_EDITOR
-    // Validate the scriptable object details entered
     private void OnValidate()
     {
         HelperUtilities.ValidateCheckNullValue(this, nameof(playButton), playButton);
@@ -68,5 +44,4 @@ public class MainMenuUI : MonoBehaviour
         HelperUtilities.ValidateCheckNullValue(this, nameof(returnToMainMenuButton), returnToMainMenuButton);
     }
 #endif
-    #endregion
 }
