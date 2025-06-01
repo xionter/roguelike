0ec9f63edb0c4f60f30024a65f1d810d1131b480
xionter <andrey0502255@gmail.com>
2025-06-01 14:54:29 +0500

    instructions added

--- Project X Echochase/Assets/Scripts/UI/MainMenuUI.cs
+++ Project X Echochase/Assets/Scripts/UI/MainMenuUI.cs
@@ -6,12 +6,12 @@ public class MainMenuUI : MonoBehaviour
     [SerializeField] private GameObject playButton;
     [SerializeField] private GameObject quitButton;
     [SerializeField] private GameObject returnToMainMenuButton;
+    [SerializeField] private GameObject instructionsButton;
 
     private void Start()
     {
-        MusicManager.Instance.PlayMusic(GameResources.Instance.mainMenuMusic, 0f, 2f);
-        SceneManager.LoadScene("MainMenuScene", LoadSceneMode.Additive);
         returnToMainMenuButton.SetActive(false);
+        playButton.SetActive(true);
     }
 
     public void PlayGame()
@@ -21,11 +21,24 @@ public class MainMenuUI : MonoBehaviour
 
     public void LoadInitial()
     {
+        SceneManager.UnloadSceneAsync("InstructionScene");
         returnToMainMenuButton.SetActive(false);
+        instructionsButton.SetActive(true);
         playButton.SetActive(true);
         quitButton.SetActive(true);
-        SceneManager.LoadScene("MainMenuScene", LoadSceneMode.Additive);
     }
+
+    public void LoadInstructions()
+    {
+        playButton.SetActive(false);
+        quitButton.SetActive(false);
+        instructionsButton.SetActive(false);
+
+        returnToMainMenuButton.SetActive(true);
+
+        SceneManager.LoadScene("InstructionScene", LoadSceneMode.Additive);
+    }
+
 
     public void QuitGame()
     {
@@ -38,6 +51,7 @@ public class MainMenuUI : MonoBehaviour
         HelperUtilities.ValidateCheckNullValue(this, nameof(playButton), playButton);
         HelperUtilities.ValidateCheckNullValue(this, nameof(quitButton), quitButton);
         HelperUtilities.ValidateCheckNullValue(this, nameof(returnToMainMenuButton), returnToMainMenuButton);
+        HelperUtilities.ValidateCheckNullValue(this, nameof(instructionsButton), instructionsButton);
     }
 #endif
 }
